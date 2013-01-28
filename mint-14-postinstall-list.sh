99410274c6e5a4c6e0a93f8f98b0039539823028
Vincent AURAT <vincent.aurat@gmail.com>
2013-01-28 19:39:52 +0100

    ajout undistract-me

--- mint-14-postinstall-list.sh
+++ mint-14-postinstall-list.sh
@@ -22,6 +22,9 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-2
 #add repository for : Oracle Java
 sudo add-apt-repository ppa:webupd8team/java 
 
+#add repository for undistract-me
+sudo add-apt-repository ppa:undistract-me-packagers/daily 
+
 ####################################################################
 #packages to install first:
 sudo apt-get install nfs-common backintime-gnome git git-core
@@ -45,7 +48,7 @@ sudo apt-get install gnome-pie conky conky-all
 sudo apt-get install firefox-locale-fr skype retext gtg geany meld planner dia calibre kmymoney gramps hyphen-fr libreoffice-filter-binfilter libreoffice-help-fr libreoffice-l10n-fr verbiste-gnome mythes-fr python-boto python-pygoocanvas ttf-mscorefonts-installer tesseract-ocr tesseract-ocr-fra tesseract-ocr-ita filezilla klavaro
 
 # network and security utlities
-sudo apt-get install htop iftop ifstat iptraf wireshark tshark arp-scan netspeed nmap netpipe-tcp chkrootkit rkhunter
+sudo apt-get install undistract-me htop iftop ifstat iptraf wireshark tshark arp-scan netspeed nmap netpipe-tcp chkrootkit rkhunter
 
 # multimedia supports 
 sudo apt-get install avidemux v4l2ucp audacity isomaster vlc x264 ffmpeg2theora oggvideotools istanbul shotwell hugin pavucontrol shutter darktable gimp gimp-plugin-registry ogmrip transmageddon guvcview wavpack mppenc faac flac vorbis-tools faad lame cheese sound-juicer picard arista milkytracker mypaint geeqie easytag radiotray banshee-extension-soundmenu banshee-extension-lyrics fotoxx phatch
